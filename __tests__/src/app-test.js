6aaf69b3117c1c2aba04d7117c640122612e4362
Vignes <vignes.arul@gmail.com>
2017-08-30 09:36:28 +0530

    Added test case to test the app module

--- __tests__/src/app-test.js
+++ __tests__/src/app-test.js
@@ -1,5 +1,15 @@
+jest.dontMock('../../src/App');
+import testUtils from 'react-addons-test-utils';
+import React from 'react';
+import App from '../../src/App';
+
 describe('the app module', () => {
   it('it should render heading', () => {
-
+    const title = 'Welcome to Jest Testing';
+    const app = testUtils.renderIntoDocument(
+      <App title = {title} />
+    );
+    const titleFromComponent = testUtils.findRenderedDOMComponentWithTag(app, 'h2');
+    expect(titleFromComponent.textContent).toEqual(title);
   })
-})
+});
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <h2>Welcome to React</h2>
+        <h2>{ this.props.title }</h2>
     );
   }
 }
